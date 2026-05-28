b1725a51d4d1df336d76d177463787ee8eccecfb
Vivek Pathak <coder.dhruvivek@gmail.com>
2026-05-28 10:28:14 +0530

    Expense routes completed

--- src/controllers/expense.controllers.js
+++ src/controllers/expense.controllers.js
@@ -24,12 +24,62 @@ export const createExpense = async(req,res) =>{
 }
 
 export const getAllExpense = async(req,res) =>{
-    
+    try{
+        const expenses = await Expense.find({user:req.user.userId});
+        res.status(200).json(expenses);
+    }
+    catch(error){
+        console.error(error);
+        res.status(500).json({message: "Internal server error"});
+    }
 
 }
 
 export const updateExpense = async(req,res)=>{
-  
+  try{
+    const expenseId = req.params.id;
+    const {title,amount,description,category,date} = req.body;
+    const expense = await Expense.findById(expenseId)
+    if(!expense){
+        return res.status(404).json({message: "Expense not found"});
+    }
+    if(expense.user.toString() != req.user.userId){
+        return res.status(401).json({message: "Unauthorized"});
+    }
+    const updatedExpense = await Expense.findByIdAndUpdate(expenseId,{
+        title,
+        amount,
+        description,
+        category,
+        date
+    },{
+        new : true,
+        runValidators : true
+    })
+    
+    res.status(200).json({message: "Expense updated successfully"});
+  }
+  catch(error){
+    console.error(error);
+    res.status(500).json({messaage: "Internal server error"});
+  }
 }
 
-export const deleteExpense = async(req,res) =>{}
+export const deleteExpense = async(req,res) =>{
+    try{
+        const expenseId = req.params.id;
+        const expense = await Expense.findById(expenseId);
+        if(!expense){
+            return res.status(404).json({message: "Expense not found"});
+        }
+        if(expense.user.toString() != req.user.userId){
+            return res.status(401).json({message: "unauthorized"});
+        }
+        await Expense.findByIdAndDelete(expenseId);
+        res.status(200).json({message : "Expense deleted successfully"});
+    }
+    catch(err){
+        console.error(err);
+        res.status(500).json({message: "Internal server error"});
+    }
+}
--- src/routes/expense.route.js
+++ src/routes/expense.route.js
@@ -5,5 +5,8 @@ import authMiddleware from '../middlewares/auth.middlewares.js';
 import{createExpense,getAllExpense,updateExpense,deleteExpense} from '../controllers/expense.controllers.js';
 
 expenseRouter.post('/create',authMiddleware,createExpense);
+expenseRouter.delete('/delete/:id',authMiddleware,deleteExpense);
+expenseRouter.patch('/update/:id',authMiddleware,updateExpense);
+expenseRouter.get('/getAll',authMiddleware,getAllExpense);
 
 export default expenseRouter;
